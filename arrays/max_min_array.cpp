4335927b0bdc94a4369dc2753f1c466ef0806f7c
Abdullah-Al-Zubaer <zubaerab5@gmail.com>
2025-04-22 02:46:03 +0600

    Update max_min_array.cpp

--- arrays/max_min_array.cpp
+++ arrays/max_min_array.cpp
@@ -1,1 +1,32 @@
+// max_min_array.cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter " << n << " elements:\n";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int maxVal = arr[0];
+    int minVal = arr[0];
+
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > maxVal)
+            maxVal = arr[i];
+        if(arr[i] < minVal)
+            minVal = arr[i];
+    }
+
+    cout << "Maximum: " << maxVal << endl;
+    cout << "Minimum: " << minVal << endl;
+
+    return 0;
+}
 
